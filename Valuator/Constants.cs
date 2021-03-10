6a7bced40b8333078fee871444d6c41f9e4f642c
JohnnyCatsvill <56757938+JohnnyCatsvill@users.noreply.github.com>
2021-03-10 20:52:28 +0300

    add lab1

---
+++ Valuator/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Valuator
+{
+    public class Constants
+    {
+        public const string RedisHost = "localhost";
+        public const int RedisPort = 6379;
+
+        public const string RankName = "RANK-";
+        public const string TextName = "TEXT-";
+        public const string SimilarityName = "SIMILARITY-";
+    }
+}
