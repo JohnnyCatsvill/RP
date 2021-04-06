213dc9060268c9e80e2eb0b74e15eaac640e5e92
JohnnyCatsvill <56757938+JohnnyCatsvill@users.noreply.github.com>
2021-04-06 19:37:51 +0300

    no more todo comments

--- Valuator/Pages/Index.cshtml.cs
+++ Valuator/Pages/Index.cshtml.cs
@@ -33,7 +33,6 @@ namespace Valuator.Pages
             string id = Guid.NewGuid().ToString();
 
             string similarityKey = Constants.SIMILARITY_NAME + id;
-            //TODO: посчитать similarity и сохранить в БД по ключу similarityKey
             {
                 int similarity = 0;
                 var keys = _storage.GetKeys(Constants.TEXT_NAME);
@@ -54,11 +53,9 @@ namespace Valuator.Pages
             }
 
             string textKey = Constants.TEXT_NAME + id;
-            //TODO: сохранить в БД text по ключу textKey
             _storage.Save(textKey, text);
 
             string rankKey = Constants.RANK_NAME + id;
-            //TODO: посчитать rank и сохранить в БД по ключу rankKey
             _broker.Publish(Constants.BROKER_CHANNEL_FOR_RANK_CALCULATION, Encoding.UTF8.GetBytes(id));
 
             return Redirect($"summary?id={id}");
