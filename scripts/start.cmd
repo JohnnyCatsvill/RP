ad22de8ff646f1c4dfe2ce38b210c83ff56af13b
JohnnyCatsvill <56757938+JohnnyCatsvill@users.noreply.github.com>
2021-04-06 19:35:07 +0300

    start script issue fixed

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -1,17 +1,23 @@
 cd ..
 cd Valuator
-start dotnet run --urls "http://localhost:5001/"
-start dotnet run --urls "http://localhost:5002/"
+dotnet build
+cd ..
+cd RankCalculator
+dotnet build
+cd ..
+
+cd ..
+cd Valuator
+start dotnet run --no-build --urls "http://localhost:5001/"
+start dotnet run --no-build --urls "http://localhost:5002/"
 
 cd ..
 cd RankCalculator
-start dotnet run
-start dotnet run
+start dotnet run --no-build
+start dotnet run --no-build
 
 cd ..
 cd ..
-
-start nats\nats-server.exe
 
 cd nginx
 start nginx.exe
