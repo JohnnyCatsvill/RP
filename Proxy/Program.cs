1e9d4c5b69268585cc83c382adc56ddc47690ba4
JohnnyCatsvill <56757938+JohnnyCatsvill@users.noreply.github.com>
2021-06-19 00:08:20 +0300

    add proxy

---
+++ Proxy/Program.cs
@@ -0,0 +1,28 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+
+namespace nngcat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Enter publishers port: ");
+            string address_pubs = System.Console.ReadLine();
+
+            System.Console.Write("Enter subscribers port: ");
+            string address_subs = System.Console.ReadLine();
+
+            using (var xpubSocket = new XPublisherSocket("@tcp://127.0.0.1:" + address_subs))
+            using (var xsubSocket = new XSubscriberSocket("@tcp://127.0.0.1:" + address_pubs))
+            {
+                Console.WriteLine("Intermediary started, and waiting for messages");
+                // proxy messages between frontend / backend
+                var proxy = new Proxy(xsubSocket, xpubSocket);
+                // blocks indefinitely
+                proxy.Start();
+            }
+        }
+    }
+}
